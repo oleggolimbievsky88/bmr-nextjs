d567ca5c76a8613b8ad5e473a422520dc6831107
Oleg Golimbievsky <oleg.golimbievsky@gmail.com>
2024-10-25 09:33:07 -0500

    Remove Menu Component

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-7d0bec059dd43216.js","static/chunks/fd9d1056-95a6fba8b83427a3.js","static/chunks/2117-1c2bd654357be796.js","static/chunks/main-app-affa94ee7bc2c777.js"],pages:{"/_app":["static/chunks/webpack-7d0bec059dd43216.js","static/chunks/framework-8e0e0f4a6b83a956.js","static/chunks/main-f0b0fa403ef884f8.js","static/chunks/pages/_app-3c9ca398d360b709.js"],"/_error":["static/chunks/webpack-7d0bec059dd43216.js","static/chunks/framework-8e0e0f4a6b83a956.js","static/chunks/main-f0b0fa403ef884f8.js","static/chunks/pages/_error-cf5ca766ac8f493f.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
