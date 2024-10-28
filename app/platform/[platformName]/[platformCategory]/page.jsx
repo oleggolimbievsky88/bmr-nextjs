81efd5944e09f00394b96e315cd9405a3eb21285
Oleg Golimbievsky <oleg.golimbievsky@gmail.com>
2024-10-28 01:06:51 -0500

    Fix paypal image and added platform page

--- app/platform/[platformName]/[platformCategory]/page.jsx
+++ app/platform/[platformName]/[platformCategory]/page.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function page() {
   return (
-    <section className="flat-spacing-9">
+    <section>
       <div className="container">
-        <div className="flat-title my-0">
+        <div>
           <h6>
             <span>
               Platform Categories
