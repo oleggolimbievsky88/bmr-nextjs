3a0c71268326db16790d9b5f7512effe3c498339
Oleg Golimbievsky <oleg.golimbievsky@gmail.com>
2025-06-16 02:10:09 -0500

    Fixed category page imports.

--- app/products/[platform]/[mainCategory]/[category]/page.jsx
+++ app/products/[platform]/[mainCategory]/[category]/page.jsx
@@ -1,5 +1,3 @@
-import Footer1 from "@/components/footers/Footer1";
-import Header2 from "@/components/headers/Header2";
 import FilterSidebar from "@/components/shop/FilterSidebar";
 import Topbar1 from "@/components/header/Topbar1";
 import React from "react";
@@ -8,16 +6,16 @@ export const metadata = {
   title: "Shop Filter Sidebar || Ecomus - Ultimate Nextjs Ecommerce Template",
   description: "Ecomus - Ultimate Nextjs Ecommerce Template",
 };
-export default function page() {
+export default function page({ params }) {
+  const { platform, mainCategory, category } = params;
   return (
     <>
       <Topbar1 />
       <div className="tf-page-title">
         <div className="container-full">
-          <div className="heading text-center">New Arrival</div>
-          <p className="text-center text-2 text_black-2 mt_5">
-            Shop through our latest selection of Fashion
-          </p>
+          <div className="heading text-center">
+            {mainCategory} - {category}
+          </div>
         </div>
       </div>
       <FilterSidebar />
